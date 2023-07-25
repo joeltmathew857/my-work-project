b4e17664530a8aac79e40f3cb3a0e681c4ae3df4
joel thomas mathew <joelmathew857@gmail.com>
2023-07-25 14:01:34 +0530

    "Initial commit"

--- src/App.js
+++ src/App.js
@@ -5,15 +5,9 @@ import TextInput from './components/TextInput';
 
 const App = () => {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-2xl font-bold">Hello, React with Tailwind CSS!</h1>
-      <p>This is a simple React component using Tailwind CSS.</p>
-
-      <Card title="Card 1" content="This is the content of Card 1." />
-      <Card title="Card 2" content="This is the content of Card 2." />
-
+    <div >
+      <Card />
       <Counter />
-
       <TextInput />
     </div>
   );
